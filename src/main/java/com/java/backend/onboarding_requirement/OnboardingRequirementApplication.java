fbd1c47b7a22c6f5e9ef8ac787d88d4b88dac9ea
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-11-08 17:06:49 +0900

    Init : Init Project
    프로젝트 생성

---
+++ src/main/java/com/java/backend/onboarding_requirement/OnboardingRequirementApplication.java
@@ -0,0 +1,13 @@
+package com.java.backend.onboarding_requirement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingRequirementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingRequirementApplication.class, args);
+	}
+
+}
