3f3528728efad4db1b5e867db3ce4e163180b55a
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-11-09 21:06:34 +0900

    Feat : #3 필요한 domain 클래스 생성
    User 정보 담은 User 클래스 생성
    User의 권한 표현하는 enum 클래스 UserRole 생성

---
+++ src/main/java/com/java/backend/onboarding_requirement/user/domain/User.java
@@ -0,0 +1,32 @@
+package com.java.backend.onboarding_requirement.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", updatable = false, nullable = false)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String nickname;
+
+    @ElementCollection
+    private List<UserRole> roles;
+
+}
