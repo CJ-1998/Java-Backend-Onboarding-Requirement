7a5f6a1e3215c51cdeaa682749d1e3e572957e40
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-11-10 21:48:11 +0900

    Chore : #7 controller swagger에 등록
    @Tag annotation으로 signup 관련 api swagger에 등록

--- src/main/java/com/java/backend/onboarding_requirement/user/controller/UserController.java
+++ src/main/java/com/java/backend/onboarding_requirement/user/controller/UserController.java
@@ -3,16 +3,18 @@ package com.java.backend.onboarding_requirement.user.controller;
 import com.java.backend.onboarding_requirement.user.dto.SignUpRequestDto;
 import com.java.backend.onboarding_requirement.user.dto.SignUpResponseDto;
 import com.java.backend.onboarding_requirement.user.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
+@Tag(name = "SignUp API", description = "회원 가입 관련 API.")
 public class UserController {
 
     private final UserService userService;
