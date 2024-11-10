8d22086a7dcfb793a673e7ae01fd1ea91010452d
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-11-10 15:26:59 +0900

    Chore : #3 security config 작성
    테스트를 위해 security config 작성
    /signup, /sign 요청 security에 걸리지 않도록 설정

---
+++ src/main/java/com/java/backend/onboarding_requirement/security/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.java.backend.onboarding_requirement.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // Spring Security 지원을 가능하게 함
+@EnableMethodSecurity(securedEnabled = true) // @Secured 애너테이션 활성화
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests
+                        .requestMatchers("/signup").permitAll() // '/signup'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/sign").permitAll() // '/sign'로 시작하는 요청 모두 접근 허가
+                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+        );
+
+        return http.build();
+    }
+}
